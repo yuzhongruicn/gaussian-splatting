3539be921ca3fab2d8a920f15647193326091f8e
yuzhongrui <yuzhongrui@baidu.com>
2023-11-02 08:54:39 +0000

    [update] add mask in bash file
    Change-Id: Ibe10f1390349039bb61d3e49e8516bf416a5d67e

--- bash/train.sh
+++ bash/train.sh
@@ -4,16 +4,32 @@ conda activate gaussian_splatting
 export PYTHONPATH="${PYTHONPATH}:/root/paddlejob/workspace/yuzhongrui/project/gaussian-splatting"
 
 args=(
-    -s /root/paddlejob/workspace/yuzhongrui/datasets/IDG_Wuhan_0907_block0       # data path
-    -m /root/paddlejob/workspace/yuzhongrui/outputs/IDG_Wuhan_block_0/init_100000     # output / model path
+    -s /root/paddlejob/workspace/yuzhongrui/datasets/IDG_Wuhan_1101_block4     # data path
+    -m /root/paddlejob/workspace/yuzhongrui/outputs/3dg/IDG_Wuhan_1101_block_4/init     # output / model path
 
     --resolution 2      # 1/2 resolution
     --eval
     --data_format idg
+    # --mask
+    --block block_4
 
-    --iterations 100000 
-    --test_iterations 7000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000
-    --save_iterations 60000 70000 80000 90000 100000
+    --iterations 50000 
+    --test_iterations 7000 20000 30000 40000 50000
+    --save_iterations 30000 40000 50000
     )
+
+# args=(
+#     -s /root/paddlejob/workspace/yuzhongrui/datasets/IDG_Wuhan_0907_block0       # data path
+#     -m /root/paddlejob/workspace/yuzhongrui/outputs/IDG_Wuhan_block_0/test     # output / model path
+
+#     --resolution 2      # 1/2 resolution
+#     --eval
+#     --data_format idg
+
+#     --mask
+
+#     --iterations 100
+#     --test_iterations 100
+#     )
 
 python train.py "${args[@]}"
