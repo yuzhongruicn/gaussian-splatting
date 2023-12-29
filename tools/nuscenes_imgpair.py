a0300d0e96e5b77bdea3c058ae66f3487a09b157
yuzhongrui <yuzhongrui@baidu.com>
2023-12-29 10:48:28 +0000

    [add] nuscenes process
    Change-Id: I3861eb2225360f97f78ad5ed0bd599891eb4675c

---
+++ tools/nuscenes_imgpair.py
@@ -0,0 +1,309 @@
+'''
+from S-NeRF
+'''
+from imageio import save
+import numpy as np
+import os
+import cv2
+import json
+from PIL import Image
+
+from scipy.spatial.transform import Rotation as R
+from pyquaternion import Quaternion
+from collections import defaultdict
+import argparse
+from utils.nuscenes_utils import transform_points, mask_outside_points, accumulate_points, points2im
+from nuscenes.nuscenes import NuScenes
+from nuscenes.utils.geometry_utils import view_points, transform_matrix
+from nuscenes.utils.data_classes import LidarPointCloud
+from copy import deepcopy
+
+import open3d as o3d
+
+
+CAMERAS = [
+    'CAM_BACK',
+    'CAM_BACK_LEFT',
+    'CAM_BACK_RIGHT',
+    'CAM_FRONT',
+    'CAM_FRONT_LEFT',
+    'CAM_FRONT_RIGHT'
+]
+
+def get_camera_params(nusc, cam_data):
+    # intrinsic
+    cam_cs_rec = nusc.get('calibrated_sensor', cam_data['calibrated_sensor_token'])
+    cam_intrinsic = np.array(cam_cs_rec['camera_intrinsic'])
+
+    # vehicle to cam
+    cam2vehicle_rotation = cam_cs_rec['rotation']
+    cam2vehicle_translation = cam_cs_rec['translation']
+    cam2vehicle = transform_matrix(cam2vehicle_translation, Quaternion(cam2vehicle_rotation))
+
+    # vehicle to global at camera timestep
+    pose_record = nusc.get('ego_pose', cam_data['ego_pose_token'])
+    vehicle2global_rotation = pose_record['rotation']
+    vehicle2global_translation = pose_record['translation']
+    vehicle2global = transform_matrix(vehicle2global_translation, Quaternion(vehicle2global_rotation))
+
+    cam2global = vehicle2global @ cam2vehicle
+
+    return cam_intrinsic, cam2global
+    
+def get_lidar_params(nusc, lidar_data):
+
+    cs_record = nusc.get('calibrated_sensor', lidar_data['calibrated_sensor_token'])
+    lidar2vehicle_rotation = cs_record['rotation']
+    lidar2vehicle_translation = cs_record['translation']
+    lidar2vehicle = transform_matrix(lidar2vehicle_translation, Quaternion(lidar2vehicle_rotation))
+    
+    # vehicle to global
+    pose_record = nusc.get('ego_pose', lidar_data['ego_pose_token'])
+    vehicle2global_rotation = pose_record['rotation']
+    vehicle2global_translation = pose_record['translation']
+    vehicle2global = transform_matrix(vehicle2global_translation, Quaternion(vehicle2global_rotation))
+    
+    # lidar to global
+    lidar2global = vehicle2global @ lidar2vehicle
+
+    return lidar2global
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', type=str, default='v1.0-mini', choices=['v1.0-mini', 'v1,0-trainval'])
+    parser.add_argument('--datadir', type=str, default = './data' )
+    parser.add_argument('--skip', type=int, default=20, help = 'Skip the first N frames') # Caution, this may vary from scene to scene
+    parser.add_argument('--total_num', type=int, default = 40, help = 'The frames needed')
+    parser.add_argument('--camera_index', type=list, default = [0,1,2,3,4,5], help = 'Cameras chosen')
+    parser.add_argument('--resize', action='store_true')
+    parser.add_argument('--savedir', type=str, default='./data/scenes')
+    parser.add_argument('--width', type=int, default=1600)
+    parser.add_argument('--height', type=int, default=900)
+    args = parser.parse_args()
+
+    
+
+    # Data Initialization
+    dataroot = os.path.join(args.datadir, 'nuScenes', args.version.split('-')[-1])
+    nusc = NuScenes(version=args.version, dataroot=dataroot, verbose=True)
+    
+    cameras = [CAMERAS[i] for i in args.camera_index]
+    scene_dict_path = os.path.join(dataroot, 'scene_dict.json')
+
+    with open(scene_dict_path, 'r') as f:
+        scene_dict = json.load(f)
+    
+    for scene_name, scene_token in scene_dict.items():
+        images, ego2global_rts, cam2ego_rts, cam_intrinsics = [], [], [], []
+        print(f"Start processing scene {scene_name} | token: {scene_token}")
+        scene = nusc.get('scene', scene_token)
+        assert(scene['name'] == scene_name), "Scene name not match"
+
+        save_dir = os.path.join(args.savedir, scene_name)
+        img_savepath = os.path.join(save_dir, 'images')
+        proj_savepath = os.path.join(save_dir, 'projcted')
+        sparse_savepath = os.path.join(proj_savepath, 'sparse')
+        dense_savepath = os.path.join(proj_savepath, 'dense')
+        os.makedirs(img_savepath, exist_ok=True)
+        os.makedirs(proj_savepath, exist_ok=True)
+        os.makedirs(sparse_savepath, exist_ok=True)
+        os.makedirs(dense_savepath, exist_ok=True)
+        
+        # sample_token = scene['first_sample_token']
+        temp_sample = nusc.get('sample', scene['first_sample_token'])
+
+        for i in range(args.skip):
+            temp_sample = nusc.get('sample', temp_sample['next'])
+        sample_token = temp_sample['token']
+            
+        IDX = 0
+        sample_idx_list = {}
+        channel_tokens = defaultdict(list)
+
+        all_points = np.empty((0, 4))
+        all_colors = np.empty((0, 3))
+
+        #get all lidar points
+        while sample_token:
+            # IDX += 1
+            sample = nusc.get('sample', sample_token)
+            lidar_token = sample['data']['LIDAR_TOP']
+            lidar_data = nusc.get('sample_data', lidar_token)
+            pointcloud = LidarPointCloud.from_file(nusc.get_sample_data_path(lidar_token))
+
+            # lidar to vehicle
+            lidar2global = get_lidar_params(nusc, lidar_data)
+            pointcloud.transform(lidar2global)
+    
+            # colors = np.empty((0, 3), dtype=np.float32)
+            colors = np.zeros((pointcloud.points.shape[1], 3), dtype=np.float32)
+            point_camera_dist = np.ones((pointcloud.points.shape[1], 1), dtype=np.float32) * np.inf
+            valid_mask_all = np.zeros((pointcloud.points.shape[1]), dtype=bool)
+
+            for cam in sample['data']:
+                if cam in cameras:
+                    cam_data = nusc.get('sample_data', sample['data'][cam])
+                    cam_img_path = nusc.get_sample_data_path(cam_data['token'])
+                    img = cv2.imread(cam_img_path)
+
+                    cam_intrinsic, cam2global = get_camera_params(nusc, cam_data)
+
+                    global2cam = np.linalg.inv(cam2global)
+
+                    pointcloud_cam = deepcopy(pointcloud)
+                    pointcloud_cam.transform(global2cam)
+                    depths = pointcloud_cam.points[2, :]
+                    points = view_points(pointcloud_cam.points[:3, :], cam_intrinsic, normalize=True)
+
+                    points_frame, depths_frame, valid_mask = mask_outside_points(points, depths, height=900)
+                    colors_frame = colors[valid_mask]
+                    point_cam_dist_frame = point_camera_dist[valid_mask]
+                    
+                    # points_img = points2im(points_frame, img)
+                    # cv2.imwrite(os.path.join(sparse_savepath, f'{cam}_{IDX:04d}_sparse.png'), points_img)
+                    # Image.fromarray(points_img).save(os.path.join(proj_savepath, f'{cam}_{IDX:04d}_proj.png'))
+
+                    for i, point in enumerate(points_frame.T):
+                        x, y = int(point[0]), int(point[1])
+                        z = depths_frame[i]
+                        if point_cam_dist_frame[i] > z:
+                            color = img[y, x, :]
+                            colors_frame[i] = color[::-1]/255
+                            point_cam_dist_frame[i] = z
+                # exit()
+                    colors[valid_mask] = colors_frame
+                    point_camera_dist[valid_mask] = point_cam_dist_frame
+                    valid_mask_all = np.logical_or(valid_mask_all, valid_mask)
+
+            all_points = np.vstack((all_points, pointcloud.points.T[valid_mask_all]))
+            all_colors = np.vstack((all_colors, colors[valid_mask_all]))
+            sample_token = sample['next']
+        
+        pointcloud_all = LidarPointCloud(all_points.T)
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(all_points[:, :3])  # xyz
+        pcd.colors = o3d.utility.Vector3dVector(all_colors)
+        o3d.io.write_point_cloud(os.path.join(save_dir, f"{scene_name}_all_points.ply"), pcd)
+        
+    # exit()
+
+
+        # #project all pcd to images
+        sample_token = temp_sample['token']
+        while sample_token:
+            sample = nusc.get('sample', sample_token)
+            for cam in sample['data']:
+                if cam in cameras:
+                    cam_data = nusc.get('sample_data', sample['data'][cam])
+                    cam_img_path = nusc.get_sample_data_path(cam_data['token'])
+                    img = cv2.imread(cam_img_path)
+                    cv2.imwrite(os.path.join(img_savepath, f'{cam}_{IDX:04d}.png'), img)
+                    proj_img = np.zeros_like(img)
+                    z_img = np.ones((img.shape[0], img.shape[1])) * np.inf
+
+                    cam_intrinsic, cam2global = get_camera_params(nusc, cam_data)
+                    global2cam = np.linalg.inv(cam2global)
+
+                    pointcloud_cam = deepcopy(pointcloud_all)
+                    pointcloud_cam.transform(global2cam)
+                    depths = pointcloud_cam.points[2, :]
+                    points = view_points(pointcloud_cam.points[:3, :], cam_intrinsic, normalize=True)
+                    points_frame, depths_frame, valid_mask = mask_outside_points(points, depths, height=900)
+
+                    for i, point in enumerate(points_frame.T):
+                        x, y = int(point[0]), int(point[1])
+                        z = depths_frame[i]
+                        if z_img[y, x] > z:
+                            proj_img[y, x, :] = img[y, x, :]
+                    
+                    cv2.imwrite(os.path.join(dense_savepath, f'{cam}_{IDX:04d}_dense.png'), proj_img)
+            IDX += 1
+            sample_token = sample['next']
+                            
+
+
+
+
+
+
+
+
+        # for s in sensor:
+        #     temp_data = nusc.get('sample_data', temp_sample['data'][s])
+        #     for i in range(args.total_num):      
+        #         data_path , _ , cam_intrinsic = nusc.get_sample_data(temp_data['token'])
+        #         if not os.path.exists(data_path):
+        #             import pdb; pdb.set_trace()
+                
+        #         channel_tokens[s].append(temp_data['token'])
+        #         if(temp_data['is_key_frame']):
+        #             sample_idx_list[IDX] = temp_data['token']
+        #         IDX += 1
+
+        #         cam_intrinsics.append(cam_intrinsic.astype(np.float32))
+        #         #image
+        #         fname = data_path
+        #         img = cv2.imread(fname)
+        #         cv2.imwrite(os.path.join(img_savepath, f'{s}_{i:04d}.png'), img)
+        #         images.append(img)
+
+        #         #ego2global
+        #         temp_ego2global = nusc.get('ego_pose',temp_data['ego_pose_token'])
+        #         ego2global_r = Quaternion(temp_ego2global['rotation']).rotation_matrix
+        #         ego2global_t = np.array(temp_ego2global['translation'])
+        #         ego2global_rt = np.eye(4)
+        #         ## correct the ego2global pose
+        #         ego2global_rt[:3,:3] = ego2global_r
+        #         ego2global_rt[:3,3] = ego2global_t
+                
+        #         ego2global_rts.append(ego2global_rt.astype(np.float32))
+        #         temp_cam2ego = nusc.get('calibrated_sensor',temp_data['calibrated_sensor_token'])
+        #         #cam2ego
+        #         cam2ego_r=Quaternion(temp_cam2ego['rotation']).rotation_matrix
+        #         cam2ego_t=np.array(temp_cam2ego['translation'])
+        #         cam2ego_rt = np.eye(4)
+        #         cam2ego_rt[:3, :3] = cam2ego_r
+        #         cam2ego_rt[:3, 3] = cam2ego_t
+        #         cam2ego_rts.append(cam2ego_rt.astype(np.float32))
+
+        #         #pcd
+        #         if s in ['CAM_FRONT']:
+        #             min_dist = 2.5
+        #         elif s in ['CAM_BACK']:
+        #             min_dist = 6
+        #         else:
+        #             min_dist = 1.5      
+                
+        #         if s in ['CAM_FRONT', 'CAM_BACK']:
+        #             frames = 50
+        #         else:
+        #             frames = 6
+        #         current_points, current_pc = map_pointcloud_to_image(nusc, temp_data['token'], 900, frames, s, min_dist)
+        #         gt = points2im(current_points)        
+        
+            
+        #         Image.fromarray(gt).save(os.path.join(proj_savepath, f'{s}_{i:04d}_proj.png'))
+                
+        #         if temp_data['next'] == '':
+        #             print(f"Sensor {s} Done, Total {len(images)} images")
+        #             break
+        #         temp_data = nusc.get('sample_data', temp_data['next'])
+
+        # camtoworlds = [ego2global_rts[i] @cam2ego_rts[i] for i in range(len(cam2ego_rts))]
+        # camtoworlds = np.stack(camtoworlds,axis=0)
+        
+        # #* Save the data
+        # save_dir = os.path.join(args.savedir, scene_name)
+        # # Change the image array to img
+        # print('the number of total images:', len(images))
+        # path = os.path.join(save_dir, 'images')
+        # os.makedirs(path,exist_ok=True)
+        # # Resize the image to the destermined resolution
+        
+        # width = args.width
+        # height = args.height
+        
+        # for i in range(len(images)):
+        #     cv2.imwrite(path+'/{:04d}.png'.format(i),images[i])
+
+        
---
+++ utils/nuscenes_utils.py
@@ -0,0 +1,126 @@
+import numpy as np
+import os.path as osp
+import cv2
+from pyquaternion import Quaternion
+from nuscenes.utils.data_classes import LidarPointCloud
+from PIL import Image
+
+MAX_WIDTH= 1600
+MAX_HEIGHT = 900
+
+def transform_points(nusc, cam, pointsensor, pc):
+    '''
+    help:
+        transform points to the target camera.
+    args:
+        nusc: An instance of class 'nusc'.
+        cam: the record of camera. (in table 'sample_data')
+        pointsensor: the record of points. (in table 'sample_data')
+        pc: A LidarPointCloud instance.
+    return:
+        depths
+    Note:
+        Calling this function will change the pc.points directly.
+    '''
+    # Points live in the point sensor frame. So they need to be transformed via global to the image plane.
+    # First step: transform the pointcloud to the ego vehicle frame for the timestamp of the sweep.
+    cs_record = nusc.get('calibrated_sensor', pointsensor['calibrated_sensor_token'])
+    pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix)
+    pc.translate(np.array(cs_record['translation']))
+
+    # Second step: transform from ego to the global frame.
+    poserecord = nusc.get('ego_pose', pointsensor['ego_pose_token'])
+    pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix)
+    pc.translate(np.array(poserecord['translation']))
+
+    # Third step: transform from global into the ego vehicle frame for the timestamp of the image.
+    poserecord = nusc.get('ego_pose', cam['ego_pose_token'])
+    pc.translate(-np.array(poserecord['translation']))
+    pc.rotate(Quaternion(poserecord['rotation']).rotation_matrix.T)
+
+    # Fourth step: transform from ego into the camera.
+    cs_record = nusc.get('calibrated_sensor', cam['calibrated_sensor_token'])
+    pc.translate(-np.array(cs_record['translation']))
+    pc.rotate(Quaternion(cs_record['rotation']).rotation_matrix.T)
+
+    # Fifth step: actually take a "picture" of the point cloud.
+    # Grab the depths (camera frame z axis points away from the camera).
+    depths = pc.points[2, :]
+    
+    return depths
+
+
+def mask_outside_points(points, depths, height):
+    '''
+    help:
+        mask the points outside the image and change the axies.
+    args:
+        points: An [2, k] array.
+        depths: An [k] array.
+        height: the height of image.
+    return:
+        points and depths after masking.
+    '''
+    # Remove points that are either outside or behind the camera. Leave a margin of 1 pixel for aesthetic reasons.
+    # Also make sure points are at least 1m in front of the camera to avoid seeing the lidar points on the camera
+    # casing for non-keyframes which are slightly out of sync.
+    mask = np.ones(depths.shape[0], dtype=bool)
+    mask = np.logical_and(mask, depths > 1.0)
+    mask = np.logical_and(mask, points[0, :] > 1)
+    mask = np.logical_and(mask, points[0, :] < MAX_WIDTH - 1)
+    mask = np.logical_and(mask, points[1, :] > MAX_HEIGHT - height + 1)
+    mask = np.logical_and(mask, points[1, :] < MAX_HEIGHT - 1)
+    points = points[:, mask]
+    depths = depths[mask]
+    
+    # Change the axies.
+    points[1, :] -= (MAX_HEIGHT - height)  
+    
+    return points, depths, mask
+
+def accumulate_points(nusc, pointsensor, type):
+    '''
+    help:
+        accumulate another point cloud file.
+    args:
+        nusc: An instance of class 'nusc'.
+        pointsensor: the record of points. (in table 'sample_data')
+        type: "next" or "prev".
+    return:
+        pointsensor_xxxx: The "next" or "prev" of the pointsensor.
+        pc_xxxx: The "next" or "prev" pointcloud.
+    '''
+    assert type in ("next", "prev"), "Invalid type. Type should be \"next\" or \"prev\"."
+    if type == "next": 
+        if not pointsensor['next']: 
+            # No next file, return with no change.
+            print("Warning: There is no more next pointcloud file.")
+            pcl_path = osp.join(nusc.dataroot, pointsensor['filename'])
+            pc = LidarPointCloud.from_file(pcl_path)
+            return pointsensor, pc
+        pointsensor_next = nusc.get('sample_data', pointsensor['next'])
+        pcl_next_path = osp.join(nusc.dataroot, pointsensor_next['filename'])
+        pc_next = LidarPointCloud.from_file(pcl_next_path) 
+        return pointsensor_next, pc_next
+    elif type == "prev":
+        if not pointsensor['prev']:
+            # No prev file, return with no change.
+            print("Warning: There is no more prev pointcloud file.")
+            pcl_path = osp.join(nusc.dataroot, pointsensor['filename'])
+            pc = LidarPointCloud.from_file(pcl_path)
+            return pointsensor, pc
+        pointsensor_prev = nusc.get('sample_data', pointsensor['prev'])
+        pcl_prev_path = osp.join(nusc.dataroot, pointsensor_prev['filename'])
+        pc_prev = LidarPointCloud.from_file(pcl_prev_path)
+        return pointsensor_prev, pc_prev   
+    
+def points2im(points, img):
+    h, w = 900, 1600
+    im = np.zeros((h, w, 3), dtype=np.uint16)
+    points_n = points.shape[1]
+    for i in range(points_n):
+        x, y = round(points[0, i]), round(points[1, i])
+        # if 0<=x<w and 0<=y<h:
+        # im[y, x] = round(points[2, i] * 256)
+        im[y, x] = img[y, x]
+    return im
